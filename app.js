1a604ec440b5ef0db24d3042d1777b0ae422abb2
bswiderski <bswiderski@LAPTOP-V6RAB314.localdomain>
2021-04-27 18:45:21 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yo Yo Yo'));
+app.get('/', (req, res) => res.send('Yo Yo Yo this is dope'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
